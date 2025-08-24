708fea759224c9e0248a7b35fc7830d49080fdc3
knowlage blast India <knowlageblastindia@gmail.com>
2025-08-24 07:43:38 +0000

    Make my project PWA compatible

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 
 import type {NextConfig} from 'next';
 
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -24,4 +28,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
